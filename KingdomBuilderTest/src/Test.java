98170663ecf955cfdada903d6d9c6df69af16f18
AshmeetC <68480568+AshmeetC@users.noreply.github.com>
2023-04-04 21:11:22 -0500

    Delete KingdomBuilderTest directory

--- KingdomBuilderTest/src/Test.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Test {
-    public static void main(String[] args) throws IOException {
-        Sub first = new Sub("board1.txt");
-        System.out.println("helo");
-    }
-}
