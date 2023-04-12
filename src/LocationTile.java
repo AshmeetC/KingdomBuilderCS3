a5bf67aae2b1aafd6035cef5e61aedfe493969d2
Ashmeet Chhabra <k1914803@students.katyisd.org>
2023-04-12 11:41:47 -0500

    git commit all

---
+++ src/LocationTile.java
@@ -0,0 +1,10 @@
+public class LocationTile {
+    private String name;
+    private boolean taken;
+    private boolean used;
+    public LocationTile(String n){
+        name = n;
+        taken = false;
+        used = false;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,36 @@
+import java.util.TreeSet;
+public class Player {
+    private int points;
+    private int playerNum;
+    private int settlements;
+    private TreeSet<LocationTile> locTiles;
+
+    public Player(int num) {
+        points=0;
+        playerNum = num;
+        settlements = 40;
+        locTiles = new TreeSet<>();
+    }
+    public int getSettlementNum() {
+        return settlements;
+    }
+    public int getPoints() {
+        return points;
+    }
+    public TreeSet getLocTiles() {
+        return locTiles;
+    }
+    public void addLocTiles(LocationTile tile) {
+        locTiles.add(tile);
+    }
+    public void removeSettlements(int num) {
+        settlements-=num;
+    }
+    public void useSettlements() {
+        settlements-=3;
+    }
+    public void score(){
+
+    }
+}
+
